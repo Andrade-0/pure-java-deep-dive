8f19a83707231e8a85003153bf8f701f0f66504e
Andrade-0 <morgadomelo01@gmail.com>
2026-06-28 23:25:00 +0100

    feat: review complete in variables

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,16 @@
 package ao.morgado;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        int age = 21, temperature = 12;
+        age = 20;
+        BigDecimal amount = BigDecimal.valueOf(2000.00); // valueOf() -> utility tool to convert one data type into another
+        String amountInString = String.valueOf(amount);
+
+        System.out.println(age);
     }
 }
