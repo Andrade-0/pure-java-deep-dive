34c361659b7d0c0113ab09bfac59704ad91e3d5f
Andrade-0 <morgadomelo01@gmail.com>
2026-07-04 14:48:25 +0100

    feat: order of operations--arithmetic expressions

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,9 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Order of Operations in arithmetic expressions
+
+        // I have already known her for years
     }
 }
