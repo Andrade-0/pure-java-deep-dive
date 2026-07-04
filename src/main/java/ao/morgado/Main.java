30503469cbf3f7a7911cf8277e067c343ae94f10
Andrade-0 <morgadomelo01@gmail.com>
2026-07-04 14:14:50 +0100

    feat: review - constants

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,7 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Constants
     }
 }
