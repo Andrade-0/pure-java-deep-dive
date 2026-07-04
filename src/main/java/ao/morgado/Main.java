44c64edcf9f7dafa92987a5d9d6f58cc6f1c701f
Andrade-0 <morgadomelo01@gmail.com>
2026-07-04 14:42:45 +0100

    feat: review - arithmetic expressions

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,33 @@
 package ao.morgado;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Arithmetic Expressions: + , - , *, /
+
+        BigDecimal amount = new BigDecimal("9999");
+
+        double price = amount.doubleValue(); // doubleValue - casting to double
+        System.out.println("Price is " + price);
+
+        Object random = 10.6;
+        double m = (double)random + 1.62;
+        System.out.println("M is " + m);
+
+        // Casting in Java is the conversion of one data type to another
+
+        int x = 12;
+        x++;
+        System.out.println("x is " + x);
+
+        int y = ++x; // it uses the new value of x after increment
+        System.out.println("y is " + y);
+
+        int w = 6;
+        w *= w; // w /= 6; || w -= 6;
+        System.out.println("w is " + w);
     }
 }
