47f80d5b61d12afc0821f75d507b467c2e8ea128
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 19:41:07 +0100

    feat: review about the ternary operator

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,14 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // ternary operator
+
+        int income = 400_000;
+        String className = income > 100_000
+                ? "First"
+                : "Economy";
+        System.out.println("Income: " + income);
+        System.out.println("Class: " + className);
     }
 }
