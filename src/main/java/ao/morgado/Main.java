8f1d68af86d2cb0e1ae213963464e48dab1913d0
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 19:03:01 +0100

    feat: review logical operators

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,12 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Logical Operators: && (and), || (or), ! (reverse the value boolean)
+
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;
+        System.out.println("Q: is warm? A: " + isWarm);
+
     }
 }
