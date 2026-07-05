a9ab3d5d831ac815ef1b4f1bef5cb1d161e4e533
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 20:18:16 +0100

    feature: implementation of the FizzBuzz test logic for  programmers

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,39 @@
 package ao.morgado;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // FizzBuzz
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Number: ");
+        int number = in.nextInt();
+
+        if((number % 3 == 0) && (number % 5 == 0))
+            System.out.println("FizzBuzz");
+        else if(number % 3 == 0)
+            System.out.println("Fizz");
+        else if(number % 5 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
+
+        // _______________________________________________ //
+
+        for (int i = 1; i <= 100; i++) {
+
+            if((i % 3 == 0) && (i % 5 == 0))
+                System.out.println("FizzBuzz");
+            else if(i % 3 == 0)
+                System.out.println("Fizz");
+            else if(i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
     }
 }
