008fba398bfbe96c565da82788ebe5d599fa7843
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 21:19:04 +0100

    feature: review about how to use do while in java, imaooo

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,22 @@
 package ao.morgado;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // do... while loops
+
+        Scanner in = new Scanner(System.in);
+
+        String voteCitizen = "";
+
+        do {
+            System.out.print("Please enter with your vote [MPLA / UNITA]: ");
+            voteCitizen = in.next().toUpperCase();
+        } while (!voteCitizen.equals("MPLA"));
+
+        System.out.print("Good vote, I'll see u after 5 years again to vote in MPLA.");
     }
 }
