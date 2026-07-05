7b0492e1419772a361599b7f84c43b0874fa27ba
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 21:14:09 +0100

    feature: review about how to use while in java, lol

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,29 @@
 package ao.morgado;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // While Loops
+
+        Scanner in = new Scanner(System.in);
+
+        String voteCitizen = "";
+
+        while (!voteCitizen.equals("MPLA")) {
+            System.out.print("Please enter with your vote [MPLA / UNITA]: ");
+            voteCitizen = in.next().toUpperCase();
+        }
+        System.out.print("Good vote, I'll see u after 5 years again to vote in MPLA.");
+
+//        int value = 0;
+//        while (value <= 7) {
+//            System.out.println(value);
+//            value++;
+//        }
+
+
     }
 }
