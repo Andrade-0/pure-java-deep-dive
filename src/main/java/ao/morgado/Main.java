8a5178331a540f12ce07d154bff7e1479a874dba
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 18:43:02 +0100

    feat: review about comparison operators to primitive types and reference types

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,15 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Comparison Operators
+
+        // to primitives types:
+        // ==, <, >, <=, >=, !=
+
+        // eg: to compare a reference type
+        String langName1 = "Java";
+        String langName2 = "PHP";
+        System.out.println(langName1.equals(langName2)); // equals is used to compare two strings
     }
 }
