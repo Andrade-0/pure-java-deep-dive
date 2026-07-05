ef7731c3a7f613aad1c3da1a06f470f590370705
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 17:27:20 +0100

    feat: i riview the data inputs

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,19 @@
 package ao.morgado;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Reading Inputs
+
+        Scanner sc = new Scanner(System.in); // A simple text scanner which can parse primitive types and strings using regular expressions
+
+        System.out.print("Please enter with your name: ");
+        String name = sc.nextLine().trim(); // .next() get only first word
+        System.out.print("Please enter with your age: ");
+        int age = sc.nextInt();
+        System.out.println("Your name: {"+name+"}"+" and your age: {"+age+"}");
     }
 }
