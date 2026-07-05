1cb023c1b36cd7af85c94566a129b801c563ec8a
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 19:24:51 +0100

    feat: reviewed about if statements

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,26 @@
 package ao.morgado;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // if statements
+
+        int temp = 23;
+
+        if (temp > 30){
+            System.out.println("Its a hot day!");
+            System.out.println("Drink Water");
+        } else if (temp > 20 && temp < 30)
+            System.out.println("Beautiful day!");
+        else
+            System.out.println("Cold day!");
+
+        double income = 400_000;
+        boolean goodIncome = (income > 350_000);
+        System.out.println(income + " " + goodIncome);
+
     }
 }
