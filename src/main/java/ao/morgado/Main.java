03126dd1d76d45a1f318e243e1548cb41176e09e
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 22:08:06 +0100

    feature: improvement error handling with while and breaks

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -19,16 +19,37 @@ public class Main {
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
         numberFormat.setCurrency(Currency.getInstance("AOA"));
 
-        System.out.print("Principal: ");
-        double amountPrincipal = in.nextDouble();
+        double amountPrincipal = 0;
+        while (true) {
+            System.out.print("Principal: ");
+            amountPrincipal = in.nextDouble();
+            if (amountPrincipal >= 10_000 &&  amountPrincipal <= 15_000000)
+                break; // stop the loop
+            System.out.println("Please enter a principal amount between 10000 and 15000");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = in.nextFloat();
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        float annualInterestRate = 0;
+        float monthlyInterestRate = 0;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = in.nextFloat();
+            if (annualInterestRate >= 1 && annualInterestRate <= 35) {
+                monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+                break; // stop the loop
+            }
+            System.out.println("Please enter a monthly interest rate between 1 and 35");
+        }
 
-        System.out.print("Period (Years): ");
-        int periodYears = in.nextInt();
-        int numberOfPayments = periodYears * MONTHS_IN_YEAR;
+        int periodYears = 0;
+        int numberOfPayments = 0;
+        while (true) {
+            System.out.print("Period (Years): ");
+            periodYears = in.nextInt();
+            if (periodYears >= 1 && periodYears <= 4) {
+                numberOfPayments = periodYears * MONTHS_IN_YEAR;
+                break;
+            }
+        }
 
         double mortgage = amountPrincipal
                 * (monthlyInterestRate * Math.pow(1 +  monthlyInterestRate, numberOfPayments))
