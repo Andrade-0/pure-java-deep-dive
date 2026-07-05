0afdb846c1fed740293b9f5df508440fc63fdfda
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 20:25:34 +0100

    feature: review about for loops and i hass been of new way to doing things with loops in arrays

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,30 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // old school
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
+
+        // new way of doing things
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        for (int i = 1; i <= 100; i++) {
+
+            if((i % 3 == 0) && (i % 5 == 0))
+                System.out.println("FizzBuzz");
+            else if(i % 3 == 0)
+                System.out.println("Fizz");
+            else if(i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
     }
 }
