045ee3631802a47ac650cf6d38aef94eb15c55c0
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 21:46:59 +0100

    feature: review about how to use for each loop in arrays

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,13 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // for each loop
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
