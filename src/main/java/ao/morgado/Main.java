d0f6a2bf6b4e9c6e6e28316c81aa04a918dd4c98
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 20:01:57 +0100

    feature: review about switch statement and learning the new syntax of a switch in java 14+

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,31 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Switch Statements
+
+        String role = "admin";
+
+        // old syntax
+        // String message;
+//        switch (role) {
+//            case "admin":
+//                message = "Youre an Admin!";
+//                break;
+//            case "manager":
+//                message = "Youre a Manager!";
+//                break;
+//            default:
+//                message = "Youre a Guest!";
+//        }
+
+        // new syntax added in java 14+
+        String message = switch (role) {
+            case "admin" -> "Youre an Admin";
+            case "manager" -> "Youre a Manager";
+            default -> "Youre a Guest";
+        };
+
+        System.out.println(message);
     }
 }
