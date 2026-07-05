30fbc19b3cc376a26c25fcb7a29e2a91c3030346
Andrade-0 <morgadomelo01@gmail.com>
2026-07-05 18:23:34 +0100

    feat: implementation a mortgage calculator super functional

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,39 @@
 package ao.morgado;
 
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Mortgage Calculator
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner in = new Scanner(System.in);
+
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        numberFormat.setCurrency(Currency.getInstance("AOA"));
+
+        System.out.print("Principal: ");
+        double amountPrincipal = in.nextDouble();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = in.nextFloat();
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        int periodYears = in.nextInt();
+        int numberOfPayments = periodYears * MONTHS_IN_YEAR;
+
+        double mortgage = amountPrincipal
+                * (monthlyInterestRate * Math.pow(1 +  monthlyInterestRate, numberOfPayments))
+                / (Math.pow(1 +  monthlyInterestRate, numberOfPayments) - 1);
+
+        System.out.println("Mortgage: " + numberFormat.format(mortgage));
     }
 }
