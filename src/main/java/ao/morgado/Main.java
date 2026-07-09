042c0a3891d8839177072c79517d442fbcbb777e
Andrade-0 <morgadomelo01@gmail.com>
2026-07-09 15:35:53 +0100

    feature: learning about lambdas

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,18 @@
 package ao.morgado;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Lambdas
+
+        List<String> numbers = Arrays.asList("one", "two", "three");
+        UnaryOperator<String> operator = String::toUpperCase;
+        numbers.replaceAll(operator);
+        System.out.println(numbers);
     }
 }
