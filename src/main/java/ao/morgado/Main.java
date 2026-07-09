9707aed06bd72f52cca88d3fa43957feb5967b1f
Andrade-0 <morgadomelo01@gmail.com>
2026-07-09 16:14:33 +0100

    feature: packages - nothing new

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -3,5 +3,9 @@ package ao.morgado;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Packages
+
+        // Nothing to study - nothing new
     }
 }
