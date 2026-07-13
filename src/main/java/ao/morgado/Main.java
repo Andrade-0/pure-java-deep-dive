3ae63769e7628c0d852991b45dfac519e1471e32
Andrade-0 <morgadomelo01@gmail.com>
2026-07-13 07:37:17 +0100

    feature: learning about exceptions

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,10 +1,16 @@
 package ao.morgado;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
 
         // Exceptions
+
+        PrintWriter out = new PrintWriter(new FileWriter("ss.s"));
 
     }
 }
