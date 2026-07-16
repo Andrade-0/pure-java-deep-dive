91ba989a3845853b854df94e1a9994823b359dc2
Andrade-0 <morgadomelo01@gmail.com>
2026-07-16 13:27:18 +0100

    feat: learning about IntSream in specific the method iterate()

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -1,7 +1,33 @@
 package ao.morgado;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I´m doing a review of Java fundamentals!");
+
+        // Converting loops with steps
+
+
+        // iterates : Returns a sequential ordered IntStream produced by iterative application of the given next
+        IntStream.iterate(
+                1,
+                i -> i <= 15,
+                i -> i + 2
+                ).forEach(System.out::println);
+
+
+        System.out.println("imperative style: ");
+        for(int i = 1;; i++){ // double semicolon means with no exist condition for this loop
+            if(i % 2 == 0){
+                break;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("functional style: ");
+        IntStream.iterate(1, i -> i + 3)
+                .takeWhile(i -> i  <= 4)
+                .forEach(System.out::println);
     }
 }
