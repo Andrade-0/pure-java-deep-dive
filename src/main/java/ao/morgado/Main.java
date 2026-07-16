47870e2dcfd0fb61c62b3e15769593fa518c92bb
Andrade-0 <morgadomelo01@gmail.com>
2026-07-16 09:12:28 +0100

    feat: deep dive about colletions type

--- src/main/java/ao/morgado/Main.java
+++ src/main/java/ao/morgado/Main.java
@@ -17,6 +17,7 @@ public class Main {
         letters.add("B");
         System.out.println(letters);
 
+        // Set
 
         // Set: A collection that contains no duplicate elements. More formally,
         // sets contain no pair of elements e1 and e2 such that e1.equals(e2)
@@ -24,7 +25,6 @@ public class Main {
         // HashSet<>() : organized list
         // LinkedHashSet<>() : preserves insertion order
         // TreeSet<>() : organized list
-
         names.add("A");
         names.add("C");
         names.add("B");
@@ -32,5 +32,27 @@ public class Main {
             System.out.println("A already exists");
         }
         System.out.println("Set list: " + names);
+
+        // Map : creates a data with key-value
+
+
+        Map<String, Integer> mapCitizens = new TreeMap<>(); // HashMap: dont accept duplicates elements
+        // HashMap
+        // LinkedHashMap : preserves insertion order
+        // TreeMap : organized list
+        // Map.of : data key-value : immutable
+        mapCitizens.put("Ana", 12); // put : to add item on list
+        mapCitizens.put("Zatetinho", 23);
+       // mapCitizens.clear(); // to clear list
+        mapCitizens.putIfAbsent("João", 17); // putIfAbsent : If the specified key is not already associated with a value
+       // mapCitizens.merge("Maria", 21, ); // ??
+        System.out.println("containsKey: " + mapCitizens.containsKey("Morgan"));
+        System.out.println(mapCitizens);
+
+        mapCitizens.forEach( (key, value) -> System.out.println(key + ": " + value)  );
+
+//        for(Map.Entry<String, Integer> entry : mapCitizens.entrySet()) { // ??
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
     }
 }
